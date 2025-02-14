5130121e0f86c550436f36fdbfed34273507f76b
Anthony Helano <anthonyhelano@outlook.com>
2025-02-14 10:13:06 -0300

    envio do projeto e readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# <div align="center"> Desafio Challenge Alura: Amigo Secreto Oracle One </div>
+
+<div align="center">
+
+
+[![GitHub Stars](https://img.shields.io/github/stars/Aklasur/amigo-secret-projeto.svg)](https://github.com/Aklasur/amigo-secret-projeto/stargazers)
+[![GitHub Forks](https://img.shields.io/github/forks/Aklasur/amigo-secret-projeto.svg)](https://github.com/Aklasur/amigo-secret-projeto/network)
+[![GitHub Issues](https://img.shields.io/github/issues/Aklasur/amigo-secret-projeto.svg)](https://github.com/Aklasur/amigo-secret-projeto/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/Aklasur/amigo-secret-projeto.svg)](https://github.com/Aklasur/amigo-secret-projeto/pulls)
+
+</div>
+
+---
+Este projeto é uma aplicação interativa simples, desenvolvida com JavaScript, HTML e CSS, criada para facilitar a organização de sorteios entre amigos. O projeto foi criado para a **Challenge Alura**, critério obrigatório na continuidade à formação **ONE (Oracle Next Education)**.
+
+## Funcionalidades
+- **Adicionar amigos**: O usuário pode inserir nomes de amigos numa lista. O botão de "Adicionar Amigo" só fica habilitado quando um nome válido é inserido.
+- **Sorteio de amigo**: Após adicionar nomes à lista, o usuário pode realizar um sorteio aleatório de um amigo e exibir o nome sorteado na tela.
+- **Feedback ao usuário**: Mensagens de alerta são exibidas para informar o usuário sobre erros, como quando não há amigos para realizar o sorteio.
+
+## Linguagens Utilizadas
+As seguintes tecnologias foram usadas no projeto:
+- **HTML**: Estrutura base da página.
+- **CSS**: Estilos personalizados e animações.
+- **JavaScript**: Responsável pela lógica do sorteio, validação e manipulação da lista.
--- app.js
+++ app.js
@@ -1,1 +1,54 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Declaração da lista e das variáveis
+let nomeAmigo = document.getElementById("amigo").value; // Campo de texto para o nome
+let listaAmigos = []; // Lista de nomes
+let resultado = document.getElementById("resultado"); // Local onde aparecerá o sorteado
+
+// Função para limpar o campo de entrada
+function limparCampo() {
+    nomeAmigo = document.querySelector("input");
+    nomeAmigo.value = "";
+}
+
+// Função de validação do nome
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector("input").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome.");
+    }
+    else {
+        listaAmigos.push(nomeAmigo);
+        console.log(listaAmigos);
+        limparCampo();
+        exibirLista();
+    } 
+}
+
+// Exibe lista na tela
+function exibirLista() {
+    // Seleciona a lista
+    let exibirLista = document.getElementById("listaAmigos");
+    // Limpa lista existente
+    exibirLista.innerHTML = "";
+    // Percorre o array e cria elementos <li> para cada amigo
+    listaAmigos.forEach(
+        amigo => {
+            let item = document.createElement("li");
+            item.textContent = amigo;
+            exibirLista.appendChild(item);
+        }
+    )
+}
+
+function sortearAmigo() {
+    // Verifica se a lista tem pelo menos um amigo
+    if (listaAmigos.length === 0) {
+        alert("A lista está vazia! Por favor, aicione amigos antes de sortear.");
+        return;
+    }
+    let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    let resultado = listaAmigos[indiceAleatorio];
+    document.getElementById("resultado").innerHTML = `Amigo sorteado: ${resultado}`;
+    console.log(sortearAmigo());
+}
